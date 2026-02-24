01afcdd91e4868a6de28956b4f903e054dc5b4bb
rootfragment <ananthanr.off@gmail.com>
2026-02-24 17:33:55 +0530

    Create 03-perf_buff.py

---
+++ eBPF_learning/03-perf_buff/03-perf_buff.py
@@ -0,0 +1,34 @@
+from bcc import BPF
+
+program = r"""
+BPF_PERF_OUTPUT(output);
+
+struct data_t{
+    int pid;
+    int uid;
+    char command[16];
+};
+int hello(void *ctx){
+    struct data_t data = {};
+    data.pid = bpf_get_current_pid_tgid() >> 32;
+    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    bpf_get_current_comm(&data.command,sizeof(data.command));
+    
+    output.perf_submit(ctx, &data , sizeof(data));
+    
+    return 0;
+}
+"""
+
+
+b = BPF(text = program)
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event = syscall , fn_name="hello")
+
+def print_event(cpu , data, size):
+    data = b["output"].event(data)
+    print(f"cpu : {cpu} pid : {data.pid} uid : {data.uid} command : {data.command.decode()}")
+
+b["output"].open_perf_buffer(print_event)
+while True:
+    b.perf_buffer_poll()
